d2318e191bd1c7da6f73449267304079910639d6
chenyue <15532196053@163.com>
2014-12-17 16:39:29 +0800

    修改百钱百鸡的测试

--- codes/specs/section_1/practice_1.js
+++ codes/specs/section_1/practice_1.js
@@ -4,21 +4,34 @@ describe('practice-1-1', function () {
         var result = hundred_hundred_chickens();
         console.dir(result);
         expect(result.length).toBe(4);
-        expect(result[0].cock).toBe(0);
-        expect(result[0].hen).toBe(25);
-        expect(result[0].chicken).toBe(75);
 
-        expect(result[1].cock).toBe(4);
-        expect(result[1].hen).toBe(18);
-        expect(result[1].chicken).toBe(78);
-
-        expect(result[2].cock).toBe(8);
-        expect(result[2].hen).toBe(11);
-        expect(result[2].chicken).toBe(81);
-
-        expect(result[3].cock).toBe(12);
-        expect(result[3].hen).toBe(4);
-        expect(result[3].chicken).toBe(84);
+        for(var i = 0; i < 4; i ++)
+        {
+            if(result[i].cock === 0)
+            {
+                expect(result[i].hen).toBe(25);
+                expect(result[i].chicken).toBe(75);
+                continue;
+            }
+            if(result[i].cock === 4)
+            {
+                expect(result[i].hen).toBe(18);
+                expect(result[i].chicken).toBe(78);
+                continue;
+            }
+            if(result[i].cock === 8)
+            {
+                expect(result[i].hen).toBe(11);
+                expect(result[i].chicken).toBe(81);
+                continue;
+            }
+            if(result[i].cock === 12)
+            {
+                expect(result[i].hen).toBe(4);
+                expect(result[i].chicken).toBe(84);
+                continue;
+            }
+        }
     });
 
 });
